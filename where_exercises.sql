1487591e0a69f7a76483e47b3bcbc53472d801cd
WilliamBaldridge <baldridge.william09@gmail.com>
2021-03-31 14:04:35 -0500

    MySQL where clauses exercise

---
+++ where_exercises.sql
@@ -0,0 +1,53 @@
+USE employees;
+
+
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+
+SELECT first_name
+FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
+
+
+SELECT first_name, gender
+FROM employees
+WHERE gender = 'M'
+  AND (
+            first_name = 'Irena'
+        OR first_name = 'Vidya'
+        OR first_name = 'Maya'
+    );
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E';
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E';
+
+
+SELECT last_name
+FROM employees
+WHERE last_name NOT LIKE '%qu%'
+  AND last_name LIKE '%q%';
+
+
